7407cf9c737ea439312d7e4247606fefcc05ef83
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-09-30 11:38:48 +0200

    Feat: Move mountains random

--- src/Mountain.js
+++ src/Mountain.js
@@ -10,6 +10,15 @@ export default class Mountain{
         return color;
     }
 
+    //create random coordinates within the window from the center
+    randomCoordinates(){
+        let randomSign = Math.random() < 0.5 ? -1 : 1;
+        let x = Math.floor(Math.random() * window.innerWidth/2 ) * randomSign;
+        let y = Math.floor(Math.random() * window.innerHeight/2) * randomSign;
+        let coords = {x: x, y: y};
+        return coords;
+    }
+ 
     render(){
         //create a div with the name of the mountain in the body
         let div = document.createElement('div');
@@ -19,6 +28,18 @@ export default class Mountain{
 
         //change the color of the mountain
         div.style.backgroundColor = this.randomColor();
+
+        //change the position of the mountain with web animation api
+        let coords = this.randomCoordinates();
+        div.animate([
+            {transform: `translate(0px, 0px)`},
+            {transform: `translate(${coords.x}px, ${coords.y}px)`},
+        ], {
+            duration: 2000,
+            iterations: 1,
+            fill: 'forwards',
+        });
+
 
 
     }
