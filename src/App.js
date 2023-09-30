13eabcce732fc5c2c3fd61eed60e9f8421043559
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-09-30 11:15:20 +0200

    Feat: render random mountain names

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+//import Mountain from './mountain.js';
+
+import Mountain from './Mountain.js';
+
 export default class App{
     constructor(){
        this.hookEvents();
@@ -9,7 +13,16 @@ export default class App{
     }
 
     addMountain(){
-        console.log("hello");
+        // array of mountain names
+        let names = ['Everest', 'K2', 'Kangchenjunga', 'Lhotse', 'Makalu', 'Cho Oyu', 'Dhaulagiri', 'Manaslu', 'Nanga Parbat', 'Annapurna'];
+
+        // get a random name from the array
+        let name = names[Math.floor(Math.random() * names.length)];
+
+        let mountain = new Mountain(name);
+
+
+        mountain.render();
     }
    
 }
---
+++ src/Mountain.js
@@ -0,0 +1,11 @@
+export default class Mountain{
+    constructor(name){
+        this.name = name;
+    }
+
+    render(){
+        console.log(this.name);
+    }
+
+   
+}
