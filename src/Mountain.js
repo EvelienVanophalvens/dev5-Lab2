a8ab5b2864e55a001bc37691beff2eeec08cb321
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-09-30 11:20:52 +0200

    Feat: create div with mountain name

--- src/Mountain.js
+++ src/Mountain.js
@@ -4,7 +4,13 @@ export default class Mountain{
     }
 
     render(){
-        console.log(this.name);
+        //create a div with the name of the mountain in the body
+        let div = document.createElement('div');
+        div.textContent = this.name;
+        div.classList.add('mountain');
+        document.body.appendChild(div);
+
+
     }
 
    
