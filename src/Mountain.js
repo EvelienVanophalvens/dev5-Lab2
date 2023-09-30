3c1ff40c95395ffe2ee7e9a10a95365cbfb01c14
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-09-30 11:25:17 +0200

    Style: give mountain random color

--- src/Mountain.js
+++ src/Mountain.js
@@ -3,12 +3,22 @@ export default class Mountain{
         this.name = name;
     }
 
+    //create random colors for the mountains
+    randomColor(){
+        let colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'black', 'brown'];
+        let color = colors[Math.floor(Math.random() * colors.length)];
+        return color;
+    }
+
     render(){
         //create a div with the name of the mountain in the body
         let div = document.createElement('div');
         div.textContent = this.name;
         div.classList.add('mountain');
         document.body.appendChild(div);
+
+        //change the color of the mountain
+        div.style.backgroundColor = this.randomColor();
 
 
     }
